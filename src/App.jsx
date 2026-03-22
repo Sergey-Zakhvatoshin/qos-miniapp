19b7ba7b2ab03141335088d71d4132a38b8a23c0
unknown <ss16zak@gmail.com>
2026-03-22 11:43:46 +0300

    Add PriceList page with routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,44 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
+import PriceList from './components/PriceList';
 import directionsImg from './assets/images/directions.webp';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('map');
+
   useEffect(() => {
     const tg = window.Telegram.WebApp;
     tg.ready();
     tg.expand();
+
+    const handleHashChange = () => {
+      const hash = window.location.hash.slice(1);
+      if (hash === 'price-list') {
+        setCurrentPage('price-list');
+      } else {
+        setCurrentPage('map');
+      }
+    };
+
+    handleHashChange();
+    window.addEventListener('hashchange', handleHashChange);
+
+    return () => {
+      window.removeEventListener('hashchange', handleHashChange);
+    };
   }, []);
 
   return (
     <div className="app">
-      <img
-        src={directionsImg}
-        alt="Location directions"
-        className="directions-image"
-      />
+      {currentPage === 'price-list' ? (
+        <PriceList />
+      ) : (
+        <img
+          src={directionsImg}
+          alt="Location directions"
+          className="directions-image"
+        />
+      )}
     </div>
   );
 }
